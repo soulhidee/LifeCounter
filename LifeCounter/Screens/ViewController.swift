1f63320d92ef7174c1301f49b9ce8f8b8899e00e
Daniil Isakov <serverpgig@yandex.ru>
2025-03-22 21:58:20 +0300

    implemented application logic

--- LifeCounter/Screens/ViewController.swift
+++ LifeCounter/Screens/ViewController.swift
@@ -8,19 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet var resultNumberLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-    @IBAction func datePicker(_ sender: UIDatePicker) {
         
     }
+    private var numberDays: Int?
     
-    @IBAction func resultButtonClicked() {
+    @IBAction func datePicker(_ sender: UIDatePicker) {
+        let calendar = Calendar.current
+        let startDate = sender.date
+        let endDate = Date()
+        
+        let components = calendar.dateComponents([.day], from: startDate, to: endDate)
+        
+        if let days = components.day {
+            numberDays = days
+            
+        }
     }
     
-
+    
+    @IBAction func resultButtonClicked(_ sender: UIButton) {
+        if let days = numberDays {
+            resultNumberLabel.text = "\(days)"
+        } else {
+            resultNumberLabel.text = "Выберите дату"
+        }
+    }
+    
+    
 }
 
