a02c14565d69f165dcf5579c88396abecf150846
Daniil Isakov <serverpgig@yandex.ru>
2025-03-22 22:06:18 +0300

    Set date restriction

--- LifeCounter/Screens/ViewController.swift
+++ LifeCounter/Screens/ViewController.swift
@@ -9,20 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet var datePicker: UIDatePicker!
     @IBOutlet var resultNumberLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        datePicker.maximumDate = Date()
     }
     private var numberDays: Int?
     
-    @IBAction func datePicker(_ sender: UIDatePicker) {
+    @IBAction func datePickerChanged(_ sender: UIDatePicker) {
         let calendar = Calendar.current
         let startDate = sender.date
         let endDate = Date()
-        
         let components = calendar.dateComponents([.day], from: startDate, to: endDate)
-        
         if let days = components.day {
             numberDays = days
             
