a1e3ac949214ec95871311b8cc9481509cd21bfa
Daniil Isakov <serverpgig@yandex.ru>
2025-03-22 21:27:48 +0300

    add @IBActions and @IBOutlet

--- LifeCounter/Screens/ViewController.swift
+++ LifeCounter/Screens/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var resultNumberLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func datePicker(_ sender: UIDatePicker) {
+        
+    }
+    
+    @IBAction func resultButtonClicked() {
+    }
+    
 
 }
 
